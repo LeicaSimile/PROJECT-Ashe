cc5467e7c0cd09f2823d15aec8c82653be3ed4f7
LeicaSimile <leicasimile@gmail.com>
2021-01-10 15:02:00 -0500

    Move hardcoded event messages to yaml config

--- bot/main/bot.py
+++ bot/main/bot.py
@@ -3,6 +3,8 @@ import datetime
 import logging
 import re
 import discord
+import yaml
+from pathlib import Path
 
 import psycopg2
 from imgurpython import ImgurClient
@@ -21,9 +23,12 @@ class Bot(object):
     def __init__(self, logger=None, **options):
         self.logger = logger or logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
+        self.events_config = None
+        with open(Path(__file__).parent.joinpath("config/events.yaml"), "r") as f:
+            self.events_config = yaml.safe_load(f.read())
+
         command_prefix = ";"
         description = settings.DESCRIPTION
-
         self.client = discord.ext.commands.Bot(command_prefix=command_prefix, description=description, **options)
 
     def run(self):
@@ -57,62 +62,6 @@ class Bot(object):
                 return False
 
             user = message.author
-            servers = {
-                533368376148361216: {
-                    "roles": {
-                        5: {
-                            "id": 533369804334039061
-                        },
-                        10: {
-                            "id": 533369803965071381
-                        },
-                        20: {
-                            "id": 533369912207474706
-                        },
-                        30: {
-                            "id": 533369949591175169
-                        },
-                        40: {
-                            "id": 783765499246936064
-                        },
-                        50: {
-                            "id": 573702137918390272
-                        },
-                        69: {
-                            "id": 655885436656549898
-                        },
-                        75: {
-                            "id": 655885354401923132
-                        },
-                        100: {
-                            "id": 655885489324294184
-                        }
-                    },
-                    "pics-only": {
-                        #548737482108174337: f"To keep #selfies clean, only posts with pictures are allowed. Feel free to post comments in #general!",
-                        538110190902312976: f"To keep #lets-draw clean, only posts with pictures are allowed. Discuss art in #lets-draw-discussion!"
-                    },
-                    "no-pics": {
-                        533377545865789441: f"To keep #general clean, posts with pictures are put in #memes-links-pics instead!",
-                        535908960713310220: f"To keep #nsfw-chat clean, posts with pictures are put in #nsfw-memes-links-pics instead!"
-                    },
-                    "no-links": {
-                        533377545865789441: f"To keep #general clean, posts with links are put in #memes-links-pics instead!",
-                        535908960713310220: f"To keep #nsfw-chat clean, posts with links are put in #nsfw-memes-links-pics instead!"
-                    }
-                },
-                662365002556243993: {
-                    "roles": {
-                        5: {
-                            "id": 668184294983991316
-                        },
-                        8: {
-                            "id": 662394541202079744
-                        }
-                    },
-                    "pics-only": {}
-                }
-            }
             if message.guild:
                 self.logger.info(f"({message.guild.name} - {message.channel.name}) {user.name}: {message.content}")
             else:
@@ -122,46 +71,38 @@ class Bot(object):
                     self.logger.info(f"({user.name}) {message.content}")
                 finally:
                     return
+
+            servers = self.events_config["servers"]
+            if message.guild.id not in servers:
+                return
             
-            if user.id == 159985870458322944 and message.guild.id in servers:  # MEE6 Bot
-                result = re.compile(r"GG <@(?:.+)>, you just advanced to level ([0-9]+)!").match(message.content)
+            events = servers[message.guild.id].get("on_message")
+            if not events:
+                return
+
+            if "level_up" in events and user.id == 159985870458322944:  # MEE6 Bot
+                result = re.compile(r"<@(?:.+)>.+level ([0-9]+)").search(message.content)
                 if result:
                     mentioned = message.mentions[0]
                     level = int(result.group(1))
                     self.logger.info(f"{mentioned.name} reached level {level}")
 
-                    roles = servers[message.guild.id].get("roles")
+                    roles = events["level_up"]["roles"]
                     for r in roles:
                         if level >= r:
                             role = discord.utils.get(message.guild.roles, id=roles[r]["id"])
                             await mentioned.add_roles(role, reason=f"User reached level {r}")
-            else:
+            elif "pics_only" in events:
                 guild = message.guild
                 channel = message.channel
 
-                if guild.id in servers and channel.id in servers[guild.id]["pics-only"]:
+                if channel.id in events["pics-only"]:
                     if await check_content(
                         message,
                         not message.attachments,
-                        servers[guild.id]["pics-only"][channel.id]
+                        events["pics-only"][channel.id]
                     ):
                         return
-                """
-                if guild.id in servers and channel.id in servers[guild.id]["no-pics"]:
-                    if await check_content(
-                        message,
-                        message.attachments,
-                        servers[guild.id]["no-pics"][channel.id]
-                    ):
-                        return
-                if guild.id in servers and channel.id in servers[guild.id]["no-links"]:
-                    if await check_content(
-                        message,
-                        re.search(URL_REGEX, message.clean_content),
-                        servers[guild.id]["no-links"][channel.id]
-                    ):
-                        return
-                """
 
             await self.client.process_commands(message)
 
@@ -192,52 +133,22 @@ class Bot(object):
     
     def event_member_update(self):
         async def on_member_update(before, after):
-            servers = {
-                533368376148361216: [
-                    {
-                        "role": 533499454964105245,
-                        "channel": "general-chat",
-                        "message": f"Welcome to the server, {after.mention}!\n- Tag your interests and unlock different areas at [#choose-roles]\n- Check out [#server-guide]\n\nIf you have any questions, feel free to message a moderator or post in [#help-and-advice]."
-                    },
-                    {
-                        "role": 533369804334039061,
-                        "channel": "general-chat",
-                        "message": f"Congrats, {after.mention}! You unlocked the [#server-portal] and the Second Floor. Check out [#serious-discussion], [#venting], and more! You can grab the NSFW role in [#choose-roles] to access the NSFW channels."
-                    },
-                    {
-                        "role": 533369912207474706,
-                        "channel": "general-chat",
-                        "message": f"Congrats, {after.mention}! You unlocked the Test Lab. Check out [#signup] to become a server tester!"
-                    },
-                    {
-                        "role": 655885436656549898,
-                        "channel": "general-chat",
-                        "message": f"{after.mention} Nice."
-                    }
-                ],
-                662365002556243993: [
-                    {
-                        "role": 662376437168472094,
-                        "channel": "general-chat",
-                        "message": f"Greetings, {after.mention}. State thy intro in [#introductions] and declare thy titles in [#roles]."
-                    }
-                ],
-                670671037343727646: [
-                    {
-                        "role": 670675588683399189,
-                        "channel": "general-chat",
-                        "message": f"Welcome to {after.guild.name}, {after.mention}! Grab some [#roles] and have fun."
-                    }
-                ]
-            }
+            servers = self.events_config["servers"]
+            if after.guild.id not in servers:
+                return
 
-            if after.guild.id in servers:
-                before_roles = [r.id for r in before.roles]
-                after_roles = [r.id for r in after.roles]
-                for milestone in servers[after.guild.id]:
-                    if milestone["role"] in after_roles and milestone["role"] not in before_roles:
-                        output_channel = discord.utils.get(after.guild.channels, name=milestone["channel"])
-                        await self.say(output_channel, milestone["message"], context=after, parse=True)
+            milestones = None
+            try:
+                milestones = servers[after.guild.id]["on_member_update"]["roles"]
+            except KeyError:
+                return
+
+            before_roles = [r.id for r in before.roles]
+            after_roles = [r.id for r in after.roles]
+            for role in milestones:
+                if role in after_roles and role not in before_roles:
+                    output_channel = discord.utils.get(after.guild.channels, name=milestones[role]["channel"])
+                    await self.say(output_channel, milestones[role]["message"], context=after, parse=True)
             
             return
         
@@ -246,7 +157,7 @@ class Bot(object):
     async def say(self, channel, message, context=None, parse=False):
         if parse and context:
             try:
-                message = message.replace("[server]", context.guild.name)
+                message = message.format(server=context.guild.name, channel=context.channel, mention=context.mention)
                 re_channels = set(re.findall(r"\[#(.+?)\]", message))
                 for c in re_channels:
                     c_object = discord.utils.get(context.guild.channels, name=c)
--- bot/test_start.py
+++ bot/test_start.py
@@ -1,3 +1,9 @@
-from main import database
+import yaml
+from pathlib import Path
+#from main import database
 
-print(database.get_all_inactive_members(1))
+events_config = None
+with open(Path(__file__).parent.joinpath("main/config/events.yaml"), "r") as f:
+    events_config = yaml.safe_load(f.read())
+
+#print(database.get_all_inactive_members(1))
