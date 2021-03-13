42d8b2bff32908aaffec8e7dcc19ee5008e4501c
LeicaSimile <leicasimile@gmail.com>
2021-03-12 23:08:41 -0500

    Add admin dao

---
+++ bot/main/commands/admin/admin_dao.py
@@ -0,0 +1,11 @@
+from main.settings import Settings
+
+def get_inactivity_settings(guild_id):
+    pass
+
+async def get_inactive_members():
+    pass
+
+async def update_inactive_members():
+    pass
+
